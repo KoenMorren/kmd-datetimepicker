5defc6d0fe282ecff11e58d4b8092f336ccfd4bb
KoenMorren <koen.morren@gmail.com>
2016-05-04 15:41:39 +0200

    complete basic functionality of the datepicker

--- source/js/datetimepicker.module.js
+++ source/js/datetimepicker.module.js
@@ -1,5 +1,22 @@
 (function() {
     'use strict';
 
-    angular.module('kmd-datetimepicker', []);
+    angular.module('kmd-datetimepicker', [])
+           .constant('FORMATS', {
+               DateFormat: 'DD-MM-YYYY',
+               SwitchLabel_Day: 'MMMM YYYY',
+               SwitchLabel_Month: 'YYYY'
+           })
+           .constant('VIEW_LEVELS', {
+               Date: {
+                   0: 'Year', Year: 0,
+                   1: 'Month', Month: 1,
+                   2: 'Day', Day: 2                   
+               },
+               DateTemplates: {
+                   0: '/source/templates/datepicker.year.html',
+                   1: '/source/templates/datepicker.month.html',
+                   2: '/source/templates/datepicker.day.html',
+               }
+           });
 })();
